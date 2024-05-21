b74ba162d8c86d03511388165c534c48ee368c99
Zulema Arteaga <zulema.arteaga@gmail.com>
2024-05-21 17:30:34 -0400

    Added test for demoqa radio buttons

---
+++ tests/how_to/test_demoqa_radio_button.py
@@ -0,0 +1,30 @@
+import pytest
+from tests.testbase import *
+import time
+
+class TestSuite:
+    @pytest.fixture
+    def precondition(self, setWebDriver: WebDriver):
+        global web, get
+        web = setWebDriver
+        get = Locators(web)
+
+        get.page("https://demoqa.com/radio-button")
+        title = web.title
+        assert title == "DEMOQA"
+
+        yield
+        web.quit()
+        
+    def test_TC1_click_buttons(self, precondition):
+        get.bySelector('.custom-control-label[for="yesRadio"').click()
+        time.sleep(3)
+        get.bySelector('.custom-control-label[for="impressiveRadio"]').click()
+        time.sleep(3)
+        rb=get.bySelector('#noRadio')
+        assert not rb.is_enabled(), "rb is enabled, which is expected"
+        time.sleep(3)
+    
+
+if __name__ == '__main__':
+    pytest.main()
